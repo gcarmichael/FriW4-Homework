7b780915e2c577886fa8659c7a75c05040832680
Gary Carmichael <g.carmichael90@gmail.com>
2016-01-01 15:53:17 +0000

    Added Category class.

--- cookbook_app/db/seeds.rb
+++ cookbook_app/db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Category.delete_all
+Recipe.delete_all
+Ingredient.delete_all
+
+Category.create!(type: 'Starter')
+Category.create!(type: 'Fish')
+Category.create!(type: 'Main')
+Category.create!(type: 'Dessert')
+
+Recipe.create!(title: 'Tomato Soup', description: 'Hearty soup for winter', instructions: 'Make Tomato Soup')
+Recipe.create!(title: 'Steak', description: 'How to perfectly cook a steak', instructions: 'Instructions for cooking steak')
+
+Ingredient.create!(title: 'Cream', type: 'Dairy')
+Ingredient.create!(title: 'Tomato', type: 'Fruit')
+Ingredient.create!(title: 'Steak', type: 'Meat')
