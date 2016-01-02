3bb0ff3f542dd9ef5ce9fc3f18b111a3fe4cf71b
Gary Carmichael <g.carmichael90@gmail.com>
2016-01-02 22:30:30 +0000

    Added views for Categories.

--- cookbook_app/db/schema.rb
+++ cookbook_app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160102211153) do
+ActiveRecord::Schema.define(version: 20160102212146) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,7 +41,6 @@ ActiveRecord::Schema.define(version: 20160102211153) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.text     "img"
-    t.integer  "category_id"
   end
 
 end
