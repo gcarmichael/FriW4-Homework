64ccecb3b4e9a977048f522ee1bcea7ee149de59
Gary Carmichael <g.carmichael90@gmail.com>
2016-01-03 17:06:23 +0000

    New schema. Removal of JoinTable and (re)addition of category_id to Recipes table.

--- cookbook_app/db/schema.rb
+++ cookbook_app/db/schema.rb
@@ -11,15 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160102212146) do
+ActiveRecord::Schema.define(version: 20160102181912) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "Categories_Recipes", id: false, force: :cascade do |t|
-    t.integer "recipe_id",   null: false
-    t.integer "category_id", null: false
-  end
 
   create_table "categories", force: :cascade do |t|
     t.text     "title"
@@ -41,6 +36,7 @@ ActiveRecord::Schema.define(version: 20160102212146) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.text     "img"
+    t.integer  "category_id"
   end
 
 end
