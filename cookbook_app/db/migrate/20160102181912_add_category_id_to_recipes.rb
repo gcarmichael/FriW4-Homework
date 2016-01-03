4378953afd1134d05e54acad1b3042e2571b3087
Gary Carmichael <g.carmichael90@gmail.com>
2016-01-03 16:57:07 +0000

    Added DropJoinTable to old migration file. Before migrating old version based on timestamp.

--- cookbook_app/db/migrate/20160102181912_add_category_id_to_recipes.rb
+++ cookbook_app/db/migrate/20160102181912_add_category_id_to_recipes.rb
@@ -1,5 +1,6 @@
 class AddCategoryIdToRecipes < ActiveRecord::Migration
   def change
     add_column :recipes, :category_id, :integer
+    drop_join_table :Categories, :Recipes, table_name: Categories_Recipes
   end
 end
